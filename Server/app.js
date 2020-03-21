c418233c0a829ad4d60cdab0f7f811fe74c9181a
Mohasin Ahamed <mohsin.ahamed@hotmail.com>
2020-03-21 17:04:41 +0530

    CORS , Login and Signup Code done

--- Server/app.js
+++ Server/app.js
@@ -11,6 +11,16 @@ const graphQlResolvers = require("./graphql/resolvers/index");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type , Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(isAuth);
 
 app.use(
@@ -26,7 +36,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mohasinac-projects-ejay1.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch(err => {
     console.log(err);
