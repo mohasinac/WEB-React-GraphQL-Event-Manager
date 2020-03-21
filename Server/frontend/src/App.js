2f2630e8ac6c1f1328327cda411d7552b6221302
Mohasin Ahamed <mohsin.ahamed@hotmail.com>
2020-03-21 18:30:19 +0530

    Context and Routing done

--- Server/frontend/src/App.js
+++ Server/frontend/src/App.js
@@ -39,6 +39,9 @@ class App extends Component {
             <main className="main-content">
               <Switch>
                 {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                {!this.state.token && (
+                  <Redirect from="/bookings" to="/auth" exact />
+                )}
                 {this.state.token && <Redirect from="/" to="/events" exact />}
                 {this.state.token && (
                   <Redirect from="/auth" to="/events" exact />
